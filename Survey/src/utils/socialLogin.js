cecc3b028a2568a798be9b324690b969181c6a99
cmilk18 <43445697+cmilk18@users.noreply.github.com>
2023-06-08 06:29:28 +0900

    nav

--- Survey/src/utils/socialLogin.js
+++ Survey/src/utils/socialLogin.js
@@ -1,6 +1,9 @@
 import React from 'react'
 
+import { useNavigate } from "react-router-dom";
+
 export default function socialLogin() {
+  
   return (
     <div>socialLogin</div>
   )
@@ -9,7 +12,9 @@ export default function socialLogin() {
 
 export function SocialLogin(provider){
   
+  const navigate = useNavigate();
+  
         navigate('/mypage');
     }
     //wind ow.location.href = API_BASE_URL + "/auth/authorize/" + provider + "?redirect_url=" + window.location.origin;
-}
+
